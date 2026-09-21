65287526f3279e4a97fbe7ce58439ce4437c2373
DuzDeveloper <damianuzcanga@gmail.com>
2026-09-21 16:42:06 -0400

    Update Init

--- backend/scrapers/__init__.py
+++ backend/scrapers/__init__.py
@@ -1,15 +1,9 @@
 """
-Scrapers para diferentes fuentes de noticias de fútbol
+Scrapers para Football News App
+Solo Goal y Marca
 """
 
-from .onefootball import scrape_onefootball
-from .marca import scrape_marca
 from .goal import scrape_goal
-from .instagram_romano import scrape_instagram_romano
+from .marca import scrape_marca
 
-__all__ = [
-    'scrape_onefootball',
-    'scrape_marca',
-    'scrape_goal',
-    'scrape_instagram_romano'
-]
+__all__ = ['scrape_goal', 'scrape_marca']
